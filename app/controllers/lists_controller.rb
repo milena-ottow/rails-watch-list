dcb7fc2b473bb73c950d2e352061830fd239e8c6
Milena Ottow <milena.ottow@protonmail.com>
2026-06-15 21:22:43 +0200

    watchlist update

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,12 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
+    @bookmark = Bookmark.new #comme le simple form est sur la show de list et qu'on a besoin d'une instance de bookmark dans le form
   end
 
   def new
@@ -20,9 +22,18 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
   private
 
-  def lists_params
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
     params.require(:list).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  root to: "lists#index" #quand je lance l'app, redirection auto sur l'index
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-  root to: "lists#index"
-  resources :lists, only: [ :index, :show, :new, :create ] do
-    resources :bookmarks, only: [ :new, :create ]
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create] #je neste car j'ai besoin de l'id de la liste pour créer un bookmark
   end
-  resources :bookmarks, only: [ :destroy ]
+  resources :bookmarks, only: :destroy #je ne neste pas car id de list inutile pour destroy le bookmark
 end
